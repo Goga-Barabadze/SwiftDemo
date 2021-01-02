8a5d21b2c6b556f552da858fdc4c14b17d74819b
Goga Barabadze <goga.barabadze73@gmail.com>
2021-01-02 16:18:11 +0100

    removed redundant return statements

--- SwiftDemo/Controller/SchoolVC+UITableViewDelegate+UITableViewDataSource.swift
+++ SwiftDemo/Controller/SchoolVC+UITableViewDelegate+UITableViewDataSource.swift
@@ -10,12 +10,11 @@ import UIKit
 extension SchoolVC: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        // One section for each class
-        return school.classes.count
+        school.classes.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return school.classes[section].students.count
+        school.classes[section].students.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
